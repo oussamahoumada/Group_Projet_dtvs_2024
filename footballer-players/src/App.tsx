54b70585248566055ad63b235be6a8f3ffea90f7
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2024-01-17 18:21:14 +0100

    add image exo3

--- footballer-players/src/App.tsx
+++ footballer-players/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import NavBar from './components/navbar/navbar'
 import GraphCountryFootballer from './components/Graph/GraphCountry'
+import Home from './components/home'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <GraphCountryFootballer />
+      <Home />
     </div>
   )
 }
--- footballer-players/src/components/home.tsx
+++ footballer-players/src/components/home.tsx
@@ -1,6 +1,13 @@
+import barcode from '../assets/images/barcode.png'
+import plot from '../assets/images/plot.png'
 
 function Home() {
-    return <div>hello</div>
+    return (
+        <div>
+            <img src={plot} alt="plot" />
+            <img src={barcode} alt="barcode" />
+        </div>
+    )
 }
 
 export default Home
