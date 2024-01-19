f6c96b3fec2e276254996af91a9e619eb3be8c1b
houmada <oussamahoumada4@gmail.com>
2024-01-19 11:43:16 +0100

    update page style

--- footballer-players/src/components/home.tsx
+++ footballer-players/src/components/home.tsx
@@ -3,6 +3,8 @@ import { getTopSw } from "../helpers/passHelpers";
 import React, { useEffect, useState } from "react";
 import ChartComponent from "./Graph/ChartComponent";
 import { getTopScorers } from "../helpers/goalsHelper";
+import Grid from '@mui/material/Grid';
+
 
 function Home() {
 
@@ -40,32 +42,36 @@ function Home() {
     }, [])
     return (
         <>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '50%', marginTop: '2%' }}>
-
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh' }}>
-                    <ChartComponent
-                        labels={labelsTkl}
-                        data={dataTkl}
-                        chartLabel='doughnut'
-                        titleChart="Les équipes qui ont le plus de joueurs blessés"
-                    />
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh', 'marginTop': '3%' }}>
-                    <ChartComponent
-                        labels={labelsTopScorers}
-                        data={dataTopScores}
-                        chartLabel='bar'
-                        titleChart="Meilleurs buteurs pour chaque compétition."
-                    />
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh', 'marginTop': '5%' }}>
-                    <ChartComponent
-                        labels={labelsTopSw}
-                        data={dataTopSw}
-                        chartLabel='line'
-                        titleChart="Graphique équipé qui ont effectué le plus de passes longues."
-                    />
-                </div>
+            <div style={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center',
+                height: '50%', marginTop: '2%'
+            }}>
+                <Grid container spacing={1} style={{justifyContent: 'center', paddingLeft: '2%'}}>
+                    <Grid item xs={4}>
+                        <ChartComponent
+                            labels={labelsTkl}
+                            data={dataTkl}
+                            chartLabel='doughnut'
+                            titleChart="Les équipes qui ont le plus de joueurs blessés"
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                            <ChartComponent
+                                labels={labelsTopScorers}
+                                data={dataTopScores}
+                                chartLabel='bar'
+                                titleChart="Meilleurs buteurs pour chaque compétition."
+                            />
+                    </Grid>
+                    <Grid item xs={10} style={{marginTop: '3%'}}>
+                            <ChartComponent
+                                labels={labelsTopSw}
+                                data={dataTopSw}
+                                chartLabel='line'
+                                titleChart="Graphique équipé qui ont effectué le plus de passes longues."
+                            />
+                    </Grid>
+                </Grid>                
             </div>
         </>
     )
