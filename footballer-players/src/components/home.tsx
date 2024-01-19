2e40cb697aff58b647ab95be3b5146cca919e313
houmada <oussamahoumada4@gmail.com>
2024-01-19 11:12:33 +0100

    update home page

--- footballer-players/src/components/home.tsx
+++ footballer-players/src/components/home.tsx
@@ -1,22 +1,71 @@
-import React from 'react'
-import plot from '../assets/images/plot.png'
-import barcode from '../assets/images/barcode.png'
-import GraphCountryFootballer from './Graph/GraphCountry'
+import { getTkl } from "../helpers/foulsHelper";
+import { getTopSw } from "../helpers/passHelpers";
+import React, { useEffect, useState } from "react";
+import ChartComponent from "./Graph/ChartComponent";
+import { getTopScorers } from "../helpers/goalsHelper";
 
 function Home() {
+
+    const [labelsTopScorers, setLabelsTopScorers] = useState<string[]>([])
+    const [dataTopScores, setDatasTopScorers] = useState<number[]>([])
+
+    const [labelsTkl, setLabelsTkl] = useState<string[]>([])
+    const [dataTkl, setDatasTkl] = useState<number[]>([])
+
+    const [labelsTopSw, setLabelsTopSw] = useState<string[]>([])
+    const [dataTopSw, setDatasTopSw] = useState<number[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const topScorers = getTopScorers()
+                const topSw = getTopSw()
+                const tkl = getTkl()
+
+                setLabelsTopScorers(topScorers.labels)
+                setDatasTopScorers(topScorers.values)
+                
+                setLabelsTkl(tkl.labels)
+                setDatasTkl(tkl.values)
+
+                setLabelsTopSw(topSw.labels)
+                setDatasTopSw(topSw.values)
+
+            } catch (error: any) {
+                console.error('Error setting JSON data:', error.message)
+            }
+        }
+
+        fetchData()
+    }, [])
     return (
         <>
-            <GraphCountryFootballer />
-            <div >
-                <h5 style={{marginTop: '5%'}} className="text-center mt-5">
-                    Visualisation des images avec Plot (Macroanalyse d'images avec JavaScript): 
-                </h5>
-                <img src={plot} alt="plot" />
-            </div><div>
-                <h5 style={{marginTop: '3%'}} className="text-center mt-5">
-                    Visualisation des images avec Barcode (Macroanalyse d'images avec JavaScript): 
-                </h5>
-                <img style={{marginBottom: '3%'}} src={barcode} alt="barcode" />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '50%', marginTop: '2%' }}>
+
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh' }}>
+                    <ChartComponent
+                        labels={labelsTkl}
+                        data={dataTkl}
+                        chartLabel='doughnut'
+                        titleChart="Les équipes qui ont le plus de joueurs blessés"
+                    />
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh', 'marginTop': '3%' }}>
+                    <ChartComponent
+                        labels={labelsTopScorers}
+                        data={dataTopScores}
+                        chartLabel='bar'
+                        titleChart="Meilleurs buteurs pour chaque compétition."
+                    />
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', height: '50vh', 'marginTop': '5%' }}>
+                    <ChartComponent
+                        labels={labelsTopSw}
+                        data={dataTopSw}
+                        chartLabel='line'
+                        titleChart="Graphique équipé qui ont effectué le plus de passes longues."
+                    />
+                </div>
             </div>
         </>
     )
